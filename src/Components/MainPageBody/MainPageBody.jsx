db2669289176ae586546800290b3b35c7d849de3
сталин <artm.zelenkin.07@mail.ru>
2024-03-20 23:32:04 +0500

    final version

--- src/Components/MainPageBody/MainPageBody.jsx
+++ src/Components/MainPageBody/MainPageBody.jsx
@@ -3,7 +3,18 @@ import styles from './styles.module.css'
 
 export function MainPageBody() {
     return (
-        <div className={styles.downPage}></div>
+        <footer className={styles.footer}>
+            <div className={styles.row}>
+                <div className={styles.ds}>
+                    <h6>Discord</h6>
+                    <iframe src="https://discord.com/widget?id=929440411758518302&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                </div>
+                <div className={styles.tg}>
+                    <h6>Telegram</h6>
+                    <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-post="rx460/416" data-width="100%" data-userpic="true" data-dark="1"></script>
+                </div>
+            </div>
+        </footer>
 
     )
 }
--- src/Components/MainPageHeader/MainPageHeader.jsx
+++ src/Components/MainPageHeader/MainPageHeader.jsx
@@ -6,16 +6,16 @@ export function MainPageHeader() {
         <a href="#">
             <img className={styles.logo} src="/server-icon.png" alt="logo" /></a>
         <ul className={styles.headerList}>
-            <li className={styles.navBarItem}><button className={styles.button}><a href="#" className={styles.href}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hdd-stack-fill" viewBox="0 0 16 16">
+            <li className={styles.navBarItem}><button className={styles.button}><a href="https://youtu.be/dQw4w9WgXcQ" className={styles.href}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hdd-stack-fill" viewBox="0 0 16 16">
                 <path d="M2 9a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-1a2 2 0 0 0-2-2H2zm.5 3a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm2 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zM2 2a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm.5 3a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm2 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1z"></path>
             </svg>Cервер</a></button></li>
-            <li className={styles.navBarItem}><button className={styles.button}><a href="#" className={styles.href}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <li className={styles.navBarItem}><button className={styles.button}><a href="https://youtu.be/dQw4w9WgXcQ" className={styles.href}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"></path>
             </svg>Персонал</a></button></li>
-            <li className={styles.navBarItem}><button className={styles.button}><a href="#" className={styles.href}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-credit-card-fill" viewBox="0 0 16 16">
+            <li className={styles.navBarItem}><button className={styles.button}><a href="https://youtu.be/dQw4w9WgXcQ" className={styles.href}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-credit-card-fill" viewBox="0 0 16 16">
                 <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v1H0V4zm0 3v5a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7H0zm3 2h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1z"></path>
             </svg>Пожертвования</a></button></li>
-            <li className={styles.navBarItem}><button className={styles.button}><a href="#" className={styles.href}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right-dots-fill" viewBox="0 0 16 16">
+            <li className={styles.navBarItem}><button className={styles.button}><a href="https://youtu.be/dQw4w9WgXcQ" className={styles.href}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right-dots-fill" viewBox="0 0 16 16">
                 <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353V2zM5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"></path>
             </svg>Поддержка</a></button></li>
         </ul></div>
